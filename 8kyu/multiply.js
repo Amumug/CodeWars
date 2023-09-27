0b98048e982281051fa1cb848a89e961c88388fc
amumu <fabriali4@gmail.com>
2023-09-27 20:52:26 +0100

    none

---
+++ 8kyu/multiply.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/50654ddff44f800200000004/solutions/javascript
+//This code does not execute properly. Try to figure out why.
+function multiply(a,b){
+    return a * b
+}
