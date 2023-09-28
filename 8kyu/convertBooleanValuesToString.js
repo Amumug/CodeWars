a726e72117203b9cf5ae802e0a74aaa303aae765
amumu <fabriali4@gmail.com>
2023-09-28 17:02:15 +0100

    CodeWars

---
+++ 8kyu/convertBooleanValuesToString.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/53369039d7ab3ac506000467/solutions/javascript
+//Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+function boolToWord( bool ){
+    //...
+    if(bool){
+        return "yes"
+    }else {
+        return "no"
+    }
+  }
