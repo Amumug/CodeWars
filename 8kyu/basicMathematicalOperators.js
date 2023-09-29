9f101454e2c12e9feb00730bd6ee60ef85004ddc
amumu <fabriali4@gmail.com>
2023-09-29 13:13:05 +0100

    CodeWars

---
+++ 8kyu/basicMathematicalOperators.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/57356c55867b9b7a60000bd7/solutions/javascript
+// Your task is to create a function that does four basic mathematical operations.
+// The function should take three arguments - operation(string/char), value1(number), value2(number).
+// The function should return result of numbers after applying the chosen operation.
+function basicOp(o, a, b){
+    return eval(a+o+b)
+}
