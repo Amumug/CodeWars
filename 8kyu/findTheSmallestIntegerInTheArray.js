8811b8722c6127d374f4c0c7847a89b43141d854
amumu <fabriali4@gmail.com>
2023-09-29 12:34:34 +0100

    CodeWars

---
+++ 8kyu/findTheSmallestIntegerInTheArray.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/55a2d7ebe362935a210000b2/solutions/javascript
+// Given an array of integers your solution should find the smallest integer.
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+        return Math.min(...args)
+    }
+}
