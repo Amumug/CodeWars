318012c31f06966ddd7287ba20dcd661824006fe
amumu <fabriali4@gmail.com>
2023-09-29 12:22:49 +0100

    CodeWars

---
+++ 8kyu/grasshoperSummation.js
@@ -0,0 +1,5 @@
+// https://codewars.com/kata/55d24f55d7dd296eb9000030/solutions/javascript
+// Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+function summation(num){
+    return num * (num + 1) / 2
+}
