20481d5c190efc56867c1d7038ff2251686d699b
Amumug <fabriali4@gmail.com>
2023-11-03 20:23:59 +0100

    codewar

--- 8kyu/removeStringSpaces.js
+++ 8kyu/removeStringSpaces.js
@@ -1,5 +1,5 @@
 // https://www.codewars.com/kata/57eae20f5500ad98e50002c5/solutions/javascript
 // Write a function that removes the spaces from the string, then return the resultant string.
 function noSpace(x){
-    return x.replaceAll(' ', '');
+    return x.split(' ').join('')
 }
