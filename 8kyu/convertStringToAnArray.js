a4bf808129af220a48fcb15b32ce9b816a75452b
Amumug <fabriali4@gmail.com>
2023-11-03 13:18:50 +0100

    codewar

---
+++ 8kyu/convertStringToAnArray.js
@@ -0,0 +1,6 @@
+//https://www.codewars.com/kata/57e76bc428d6fbc2d500036d/solutions/javascript
+// Write a function to split a string and convert it into an array of words.
+
+function stringToArray(string){
+    return string.split(' ')
+}
