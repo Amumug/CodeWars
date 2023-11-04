6502f7b20ba37ab385649ea601456bdb44be0537
Amumug <fabriali4@gmail.com>
2023-11-04 13:03:25 +0100

    codewar

---
+++ 8kyu/catYearsDogYears.js
@@ -0,0 +1,26 @@
+// https://www.codewars.com/kata/5a6663e9fd56cb5ab800008b/solutions/javascript
+// I have a cat and a dog.
+
+// I got them at the same time as kitten/puppy. That was humanYears years ago.
+
+// Return their respective ages now as [humanYears,catYears,dogYears]
+
+// NOTES:
+
+// humanYears >= 1
+// humanYears are whole numbers only
+// Cat Years
+// 15 cat years for first year
+// +9 cat years for second year
+// +4 cat years for each year after that
+// Dog Years
+// 15 dog years for first year
+// +9 dog years for second year
+// +5 dog years for each year after that
+
+var humanYearsCatYearsDogYears = function(y){
+    if(y == 1) return [1, 15, 15]
+    if(y == 2) return [2, 24, 24]
+
+    return (y, (y-1) * 4 + 24, (y-2) * 5 + 24)
+}
