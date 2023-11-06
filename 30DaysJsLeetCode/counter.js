3b47666ba9a7d33be41b84c294600a9fc1dede5c
Amumug <fabriali4@gmail.com>
2023-11-06 07:44:39 +0100

    30 day js LeetCode

---
+++ 30DaysJsLeetCode/counter.js
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/counter/description/
+// Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+
+const createCounter = (n) => {
+    let count = n
+    return  () => {
+        return count++;
+    }
+}
