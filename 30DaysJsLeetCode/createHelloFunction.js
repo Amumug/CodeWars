0835bb03499d0f9a7fc16f67cf6c05a602e0ce47
Amumu <126900757+Amumug@users.noreply.github.com>
2023-11-10 21:23:27 +0100

    Delete 30DaysJsLeetCode directory

--- 30DaysJsLeetCode/createHelloFunction.js
+++
@@ -1,6 +0,0 @@
-// https://leetcode.com/problems/create-hello-world-function/description/?envType=study-plan-v2&envId=30-days-of-javascript
-// Write a function createHelloWorld. It should return a new function that always returns "Hello World".
-
-const createHelloWorld = function(){
-    return () => "Hello World"
-}
