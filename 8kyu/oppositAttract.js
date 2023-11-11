f05318f29a7161f9e63f79e363973dccdcd163c8
Amumug <fabriali4@gmail.com>
2023-11-11 08:27:50 +0100

    CodeWars

--- 8kyu/oppositAttract.js
+++ 8kyu/oppositAttract.js
@@ -2,5 +2,5 @@
 // Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each. If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
 
 function loveflo(flower1, flower2){
-    return flower1 % 2 !== flower2 % 2
+    return flower1 % 2 !== flower2 % 2;
 }
