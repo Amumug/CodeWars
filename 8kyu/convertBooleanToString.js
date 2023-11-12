877aebcecf23aaf83fc07927f0f6b80607f2c286
Amumug <fabriali4@gmail.com>
2023-11-12 08:23:16 +0100

    CodeWars

---
+++ 8kyu/convertBooleanToString.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/551b4501ac0447318f0009cd/solutions/javascript
+// Implement a function which convert the given boolean value into its string representation.
+
+function booleanToString(b){
+    return b === true ? "true" : "false"
+}
---
+++ 8kyu/removeExlamationMarks.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/57a0885cbb9944e24c00008e/solutions/javascript
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+function RemoveExclamationMarks(str){
+    return str.split('!').join('')
+}
