d8f5b61fb0b2311984033932a076c60c21fbc5ad
Amumug <fabriali4@gmail.com>
2023-11-12 08:24:16 +0100

    convertBooleanToString

--- 8kyu/convertBooleanToString.js
+++ 8kyu/convertBooleanToString.js
@@ -2,5 +2,5 @@
 // Implement a function which convert the given boolean value into its string representation.
 
 function booleanToString(b){
-    return b === true ? "true" : "false"
+    return b === true ? "true" : "false";
 }
