895afe89af533a3bb7744822035bae01a4d3a2de
Amumug <fabriali4@gmail.com>
2023-11-13 12:16:47 +0100

    doIgetBonus

---
+++ 8kyu/doIgetBonus.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/56f6ad906b88de513f000d96/train/javascript
+// It's bonus time in the big city! The fatcats are rubbing their paws in anticipation... but who is going to make the most money?
+
+function bonusTime(salary, bonus){
+    return bonus === true ? `£${salary * 10}` : `£${salary * 1}`
+}
