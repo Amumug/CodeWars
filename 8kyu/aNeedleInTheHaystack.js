3807b9b168f04721081e20b63431f4a91a5d184e
Amumug <fabriali4@gmail.com>
2023-11-14 12:22:00 +0100

    A Needle in the Haystack

---
+++ 8kyu/aNeedleInTheHaystack.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/56676e8fabd2d1ff3000000c/train/javascript
+// Write a function findNeedle() that takes an array full of junk but containing one "needle"
+function findNeedle(haystack){
+    return `found the needle at position ${haystack.indexOf('needle')}`
+}
