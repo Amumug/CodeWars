a7a6dd41514a696e1a4c2c656aad34c8c303d901
Amumug <fabriali4@gmail.com>
2023-11-15 20:06:21 +0100

    You only need one

---
+++ 8kyu/youOnlyNeedOne.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/57cc975ed542d3148f00015b/train/javascript
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+function check(a, x){
+    return a.includes(x)
+}
