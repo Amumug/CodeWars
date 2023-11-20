e3e31f824cd268284adb25f46c15f194e9937baf
Amumug <fabriali4@gmail.com>
2023-11-20 12:48:59 +0100

    Simple Fun #261: Whose Move

--- 8kyu/simpleFun#261:WhoseMove.js
+++ 8kyu/simpleFun#261:WhoseMove.js
@@ -2,5 +2,5 @@
 // Two players - "black" and "white" are playing a game. The game consists of several rounds. If a player wins in a round, he is to move again during the next round. If a player loses a round, it's the other player who moves on the next round. Given whose turn it was on the previous round and whether he won, determine whose turn it is on the next round.
 
 functio function whoseMove(lastPlayer, win) {
-    return lastPlayer === "white" && win === true ? "white" : lastPlayer === "black" && win === "false" ? "white" : "black"
+    return lastPlayer === "white" && win === true ? "white" : lastPlayer === "black" && win === "false" ? "white" : "black";
 }
