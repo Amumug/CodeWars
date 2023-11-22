1a7a3edbdcf6acdb1d6065ea5088f206f4ff2dc2
Amumug <fabriali4@gmail.com>
2023-11-22 11:20:52 +0100

    Gravity Flip

---
+++ 8kyu/gravityFlip.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/5f70c883e10f9e0001c89673/train/javascript
+// If you've completed this kata already and want a bigger challenge, here's the 3D version
+
+function flip(a,d){
+    if(d === 'R') return a.sort((a,b) => a - b);
+    if(d === 'R') return a.sort((a,b) => b - a);
+}
