56540ee397346a576c33b92fb92670f004ae64fe
Amumug <fabriali4@gmail.com>
2023-11-23 17:52:17 +0100

    Reversed Word

---
+++ 8kyu/reversedWord.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/51c8991dee245d7ddf00000e/train/javascript
+// Complete the solution so that it reverses all of the words within the string passed in.
+
+function reverseWord(str){
+    return str.split(' ').reverse().join(' ')
+}
