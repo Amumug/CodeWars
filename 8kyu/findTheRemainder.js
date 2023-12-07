9d97775e13a8a9bbbbd6aae85a9d7d9c410ea824
Amumug <fabriali4@gmail.com>
2023-12-07 11:24:05 +0100

    Find the Remainder

---
+++ 8kyu/findTheRemainder.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/524f5125ad9c12894e00003f/train/javascript
+// Write a function that accepts two integers and returns the remainder of dividing the larger value by the smaller value.
+
+function remainder(n,m){
+    return n > m ? n % m : m % n
+}
