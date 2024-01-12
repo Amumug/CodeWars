77753696652cc959d54d940baed5ce44e74addba
root <root@DESKTOP-FLL3EFF>
2024-01-12 08:25:41 +0100

    CodeWars

--- 8kyu/FuelCalculatorTotalCost.js
+++ 8kyu/FuelCalculatorTotalCost.js
@@ -9,5 +9,5 @@ function fuelPrice(litres, pricePerLitre) {
     for(let i = 2; i <= 10; i += 2)
       if(litres >= i)
         pricePerLitre -= .05
-      return +(litres * pricePerLitre).toFixed(2)
+      return +(litres * pricePerLitre).toFixed(2);
 }  
