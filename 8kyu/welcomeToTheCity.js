f16f2d4bb439e07aa535d85a693aec216d970869
root <root@DESKTOP-FLL3EFF>
2024-01-12 22:30:30 +0100

    CodeWars

--- 8kyu/welcomeToTheCity.js
+++ 8kyu/welcomeToTheCity.js
@@ -1,5 +1,5 @@
 // https://www.codewars.com/kata/5302d846be2a9189af0001e4/train/javascript
 // Create a method that takes as input a name, city, and state to welcome a person. Note that name will be an array consisting of one or more values that should be joined together with one space between each, and the length of the name array in test cases will vary.
 function sayHello(name, city, state){
-    return `Hello, ${name.join(' ')}! Welcome to ${city}, ${state}!`;
+    return `Hello, ${name.join(' ')}! Welcome to ${city}, ${state}!`
 }
