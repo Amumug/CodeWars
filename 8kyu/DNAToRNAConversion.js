a3e5e5b5330941a129ee14d8a4fe46dc59811b20
Amumug <fabriali4@gmail.com>
2024-02-01 09:25:49 +0100

    CodeWars

--- 8kyu/DNAToRNAConversion.js
+++ 8kyu/DNAToRNAConversion.js
@@ -2,5 +2,5 @@
 // Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
 
 function DNAtoRNA(dna) {
-    return dna.split('T').join('U')
+    return dna.split('T').join('U');
 }
